ae153bc2e7f4b758abe0588a6af70c977a1f4dd0
German M. Bravo <german.mb@gmail.com>
2018-06-22 11:57:50 -0500

    Added info for Scala (dotty), Rust and Go

--- src/build.sh
+++ src/build.sh
@@ -33,6 +33,7 @@ pip3 install python-language-server -t dist/python
 pip3 install pyflakes -t dist/python
 pip3 install rope -t dist/python
 pip3 install autopep8 -t dist/python
+# pip3 install pyls-mypy -t dist/python
 
 pip install future -t dist/python/compat
 pip install configparser -t dist/python/compat
@@ -75,6 +76,36 @@ cd ../../..
 # cd plugins
 # ln -fs org.eclipse.equinox.launcher_*.jar org.eclipse.equinox.launcher.jar
 # cd ../../..
+
+########################################################################
+# Scala LS
+./coursier fetch --cache dist/scala -p ch.epfl.lamp:dotty-language-server_0.8:0.8.0
+ln -fs ../../coursier dist/scala/coursier
+# java -jar dist/scala/coursier launch --cache dist/scala ch.epfl.lamp:dotty-language-server_0.8:0.8.0 -M dotty.tools.languageserver.Main -- -stdio  # <- starts server
+
+
+# External Servers:
+
+########################################################################
+# Rust LS
+brew install rustup
+export PATH="~/.cargo/bin:$PATH"
+rustup default nightly
+rustup component add rls-preview
+# rustup run nightly rls  # <- starts server
+
+########################################################################
+# Go LS
+brew install go
+export PATH=":~/go/bin:$PATH"
+go get -u github.com/sourcegraph/go-langserver
+go get -u github.com/nsf/gocode
+# go-langserver  # <- starts server
+
+########################################################################
+# R LS
+brew install r
+# R --quiet --slave -e languageserver::run()  # <- starts server
 
 ########################################################################
 noglob rsync --exclude .git --exclude '*.tar.gz' --exclude '*.pyc' --exclude '__pycache__' --exclude 'bin' -av --delete --copy-links dist/ ../servers
